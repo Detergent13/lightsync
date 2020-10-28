2072cc6db54e8026eb68bcfbe50d0654c5d7c5ff
Detergent13 <50428606+Detergent13@users.noreply.github.com>
2020-10-27 22:18:42 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-print("hi")
-
-while True:
-    os.system("iobroker state set tuya.0.85873706cc50e3c9e7a4.5 ff00000000ffff")
-    os.system("iobroker state set tuya.0.85873706cc50e3c9e7a4.5 00ff000000ffff")
-    os.system("iobroker state set tuya.0.85873706cc50e3c9e7a4.5 0000ff0000ffff")
